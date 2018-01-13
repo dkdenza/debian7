29c823e8d1f236ef02bd40e16337ca745d7605a4
dkdenza <31194176+dkdenza@users.noreply.github.com>
2018-01-13 15:35:27 +0000

    Update x.sh

--- x.sh
+++ x.sh
@@ -8,7 +8,7 @@ fi
 # initialisasi var
 export DEBIAN_FRONTEND=noninteractive
 OS=`uname -m`;
-#MYIP=$(wget -qO- https://raw.githubusercontent.com/dkdenza/debian7/master/ip.txt);
+#MYIP=$(wget -qO- );
 
 # get the VPS IP
 #ip=`ifconfig venet0:0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
@@ -16,7 +16,7 @@ OS=`uname -m`;
 #MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
 MYIP=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
 if [ "$MYIP" = "" ]; then
-	MYIP=$(wget -qO- https://raw.githubusercontent.com/dkdenza/debian7/master/ip.txt)
+	MYIP="45.32.109.145"
 fi
 MYIP2="s/xxxxxxxxx/$MYIP/g";
 ether=`ifconfig | cut -c 1-8 | sort | uniq -u | grep venet0 | grep -v venet0:`
